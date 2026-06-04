13d856468b20fbaf5da1ff531f264c69c0cf5f2a
MuxsinjonCoder <muxsincoder@gmail.com>
2026-06-04 18:01:17 +0500

    feat: header items added

--- src/widgets/swot/header.vue
+++ src/widgets/swot/header.vue
@@ -1,9 +1,31 @@
 <template>
-  <section class="container py-5">
-    <Input placeholder="Name your swot analysis..." />
+  <section class="container py-5 flex items-center justify-between gap-5 flex-wrap md:flex-nowrap">
+    <Input class="md:max-w-[50%]" placeholder="Name your swot analysis..." />
+
+    <div class="flex items-center gap-3">
+      <Button variant="ghost"> <FolderDown class="size-4" /> Import</Button>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button variant="ghost"> <FolderUp class="size-4" /> Export</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>as image</DropdownMenuItem>
+          <DropdownMenuItem>as JSON</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   </section>
 </template>
 
 <script setup lang="ts">
+import { FolderDown, FolderUp } from "@lucide/vue";
+import Button from "~/shared/components/ui/button/button.vue";
 import Input from "~/shared/components/ui/input/Input.vue";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "~/shared/components/ui/dropdown-menu";
 </script>

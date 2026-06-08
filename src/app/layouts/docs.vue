85e8c8ac246a9d1030fbdb514999e601a25336a3
MuxsinjonCoder <muxsincoder@gmail.com>
2026-06-08 09:45:46 +0500

    feat: left styles added

--- src/app/layouts/docs.vue
+++ src/app/layouts/docs.vue
@@ -16,11 +16,14 @@ import SiteFooter from "~/widgets/layout/footer/site-footer.vue";
 </script>
 
 <style lang="scss" scoped>
+.docs-page {
+  min-height: 100vh;
+  background-color: var(--bg-base);
+}
+
 .layout {
   display: flex;
   align-items: start;
   gap: 20px;
-  min-height: 100vh;
-  background-color: var(--bg-base);
 }
 </style>
